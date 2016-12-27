a7b82d0a0a0f47e4556774a8aa7bf7e68c50b318
Paul-Antoine <palevasseur@gmail.com>
2016-12-27 18:53:21 +0100

    project init with react, babel, webpack

---
+++ README.md
@@ -0,0 +1,2 @@
+build: "npm run build"
+launch: open index.html in browser
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: './index.jsx',
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: './node_modules/',
+                loader: 'babel'
+            }
+        ]
+    }
+};
