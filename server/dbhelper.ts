f0abb1f5058786235f2fdcfeed7406eddac94d88
Paul-Antoine <palevasseur@gmail.com>
2017-01-23 21:59:49 +0100

    add mongoose

--- server/dbhelper.ts
+++ server/dbhelper.ts
@@ -1,3 +1,5 @@
+var mongoose = require('mongoose');
+
 /*
 [
     {
@@ -14,11 +16,60 @@
 ]
 */
 
+var conf = {
+    db: {
+        cnx: "mongodb://localhost/test",
+        photo: {
+            modelName: "Photo",
+            schema: {
+                fileName: String
+            }
+        }
+    }
+};
+
+var Photo = null;
+(function initDB() {
+    mongoose.connect(conf.db.cnx);
+    var db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'mongodb connection error:'));
+    db.once('open', function() {
+        // create model
+        var dbSchema = mongoose.Schema(conf.db.photo.schema);
+        Photo = mongoose.model(conf.db.photo.modelName, dbSchema);
+    });
+}) ();
+
 export function refreshDB(photosList) {
+    if(!Photo) {
+        return;
+    }
+
+    console.log('refreshDB:');
+
+    // create photos
     photosList.forEach(ref => {
-        console.log('piece:' + ref.refPiece);
+        //console.log('piece:' + ref.refPiece);
         ref.listPhotos.forEach(photo => {
-            console.log('  photo:' + photo.nomFichier);
+            Photo.find({fileName: photo.nomFichier}, (err, photosFound) => {
+                photosFound = photosFound || 0;
+
+                if(photosFound.length == 0) {
+                    console.log('=> Import new photo "' + photo.nomFichier + '"');
+                    let newPhoto = new Photo({ fileName: photo.nomFichier });
+                    newPhoto.save((err, fluffy) => {if (err) return console.error(err)});
+                }
+
+                if(photosFound.length > 1) {
+                    console.log('=> Found several instances of :' + photo.nomFichier + "\n", photosFound);
+                }
+
+                if(photosFound.length == 1) {
+                    //console.log('=> Already exist :' + photo.nomFichier);
+                }
+
+            });
         })
     });
+
 }
